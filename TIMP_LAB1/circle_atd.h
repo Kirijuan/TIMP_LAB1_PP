3d2bce749b64b9b6cb60a8cc7540c85a407d3cee
Кирилл Селютин <kir.seliutin1@outlook.com>
2019-10-27 21:26:26 +0700

    Добавьте файлы проекта.

---
+++ TIMP_LAB1/circle_atd.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef __triangle_atd__
+#define __triangle_atd__
+namespace simple_shapes {
+	// круг
+	struct circle {
+		int a, b; // целочисленные координата центра окружности,
+			  // радиус
+		char colour[10];
+	};
+} // end simple_shapes namespace
+#endif
